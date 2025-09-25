573ab37c0eae8469ed63d4b5cbfaef0e2b4790be
Jack17kdb <jack@kali.Johnson.com>
2025-09-25 12:39:05 +0300

    project setup

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express()
+
+app.get("/", (req, res) => {
+	res.status(200).json({msg: "Connected successfully"})
+})
+
+app.listen(PORT, () => {
+	console.log(`Server started on port: ${PORT}`);
+});
